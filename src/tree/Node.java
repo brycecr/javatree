e26965e7fe51d7843c6ba988625a0cfd8c15e148
brycecr <brycecr@gmail.com>
2013-08-07 22:34:17 -0700

    basic tree framework

---
+++ src/tree/Node.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2013 gestalt.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tree;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author brycecr
+ */
+public class Node<T> {
+
+	protected ArrayList<Node<T>> children = new ArrayList<Node<T>>();
+	protected Node<T> parent = null;
+	protected T data;
+
+	public Node(T id) {
+		data = id;
+	}
+
+	public Node(T id, ArrayList<Node<T>> initchildren) {
+		children = initchildren;
+		data = id;
+	}
+
+	public Node(T id, Node<T> initparent) {
+		parent = initparent;
+		data = id;
+	}
+
+	public Node(T id, Node<T> initparent, ArrayList<Node<T>> initchildren) {
+		parent = initparent;
+		children = initchildren;
+		data = id;
+	}
+
+	/**
+	 * 
+	 * @return parent node of this node. May be null.
+	 */
+	public Node<T> getParent() {
+		return parent;
+	}
+
+	/**
+	 * @return ArrayList of children of this node
+	 */
+	public ArrayList<Node<T>> getChildren() {
+		assert children != null;
+		return children;
+	}
+
+	public void addChild(Node<T> newchild) {
+		children.add(newchild);
+	}
+
+	public void addParent(Node<T> newparent) {
+		assert parent == null;
+		if (parent == null) {
+			parent = newparent;
+		}
+	}
+}
